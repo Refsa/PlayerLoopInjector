23d8f395138ca525105e4b40549c746012af2b5e
Ole <refsa22@gmail.com>
2021-06-08 15:18:14 +0200

    sample: added samples

---
+++ Samples/InjectedComponent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerLoopInjector.Samples
+{
+    public class InjectedComponent : MonoBehaviour, IPlayerLoop
+    {
+        void OnEnable()
+        {
+            Injector.Inject(this);
+        }
+
+        void OnDisable()
+        {
+            Injector.Remove(this);
+        }
+
+        public void PlayerLoopInitialization()
+        {
+            Debug.Log("PlayerLoopInitialization");
+        }
+
+        public void PlayerLoopEarlyUpdate()
+        {
+            Debug.Log("PlayerLoopEarlyUpdate");
+        }
+
+        public void PlayerLoopPreUpdate()
+        {
+            Debug.Log("PlayerLoopPreUpdate");
+        }
+
+        public void PlayerLoopUpdate()
+        {
+            Debug.Log("PlayerLoopUpdate");
+        }
+
+        public void PlayerLoopPostUpdate()
+        {
+            Debug.Log("PlayerLoopPostUpdate");
+        }
+
+        public void PlayerLoopFixedUpdate()
+        {
+            Debug.Log("PlayerLoopFixedUpdate");
+        }
+
+        public void PlayerLoopEndOfFrame()
+        {
+            Debug.Log("PlayerLoopEndOfFrame");
+        }
+    }
+}
